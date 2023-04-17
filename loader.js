a1b13e4daf3eca552179d79a2e90b7da08f5bcca
Craexz <73430720+Craexz@users.noreply.github.com>
2023-04-17 12:38:18 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -15,7 +15,7 @@ var bar = document.createElement("div");
 bar.style.cssText = `
     position: fixed;
     z-index: 999999;
-    height: 10px;
+    height: 15px;
     width: 600px;
     top: ${iframe.offsetTop}px;
     left: ${iframe.offsetLeft}px;
@@ -25,6 +25,37 @@ bar.style.cssText = `
 document.body.appendChild(bar);
 
 
+var minimizeButton = document.createElement("div");
+minimizeButton.style.cssText = `
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 20px;
+    height: 15px;
+    background-color: #666;
+    cursor: pointer;
+`;
+bar.appendChild(minimizeButton);
+
+
+var reopenButton = document.createElement("div");
+reopenButton.style.cssText = `
+    position: fixed;
+    z-index: 999999;
+    width: 100px;
+    height: 50px;
+    bottom: 10px;
+    right: 10px;
+    background-color: #666;
+    color: #fff;
+    text-align: center;
+    line-height: 50px;
+    cursor: pointer;
+    display: none;
+`;
+reopenButton.innerHTML = "Open UI";
+document.body.appendChild(reopenButton);
+
 var isDragging = false;
 var dragStartX, dragStartY;
 
@@ -33,6 +64,7 @@ bar.addEventListener("mousedown", function(event) {
     dragStartX = event.clientX;
     dragStartY = event.clientY;
 });
+
 
 document.addEventListener("mouseup", function(event) {
     isDragging = false;
@@ -52,9 +84,25 @@ document.addEventListener("mousemove", function(event) {
 });
 
 
+minimizeButton.addEventListener("click", function(event) {
+    iframe.style.display = "none";
+	topbar.style.visibility = "hidden";
+    reopenButton.style.display = "block";
+});
+
+
+reopenButton.addEventListener("click", function(event) {
+    iframe.style.display = "block";
+	topbar.style.visibility = "visible";
+    reopenButton.style.display = "none";
+});
+
+
+
 var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
 
 var script = iframeDoc.createElement('script');
 script.src = 'https://craexz.github.io/mortal-hub/raw.js';
 iframeDoc.head.appendChild(script);
+
