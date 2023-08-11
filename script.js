d50fafab372691e21d272df7d2cbc62a4df94b78
Craexz <73430720+Craexz@users.noreply.github.com>
2023-08-10 17:37:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -781,7 +781,7 @@ function main() {
       });
       return button;
     }
-}
+
 
 var overlay = document.createElement("div");
 overlay.style.cssText = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.8); backdrop-filter: blur(5px); z-index: 9999;";
